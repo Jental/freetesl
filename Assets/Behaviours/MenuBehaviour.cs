67c4adf5373c558a15cc89339f665551f9e9ebac
Jental <jentalmeister@gmail.com>
2025-03-07 12:31:42 +0300

    Menu close button

--- Assets/Behaviours/MenuBehaviour.cs
+++ Assets/Behaviours/MenuBehaviour.cs
@@ -10,12 +10,16 @@ namespace Assets.Behaviours
     {
         [SerializeField] private Button[] buttons = Array.Empty<Button>();
         [SerializeField] private GameObject? buttonsBlock = null;
+        [SerializeField] private Button? closeButton = null;
         
         private bool isRendered = false;
 
         protected void Start ()
         {
             if (buttonsBlock == null) throw new InvalidOperationException($"{nameof(buttonsBlock)} game object is expected to be set");
+            if (closeButton == null) throw new InvalidOperationException($"{nameof(closeButton)} game object is expected to be set");
+
+            closeButton.onClick.AddListener(OnCloseClick);
         }
 
         protected void OnDisable()
@@ -34,6 +38,15 @@ namespace Assets.Behaviours
                     button.gameObject.transform.parent = buttonsBlock!.transform;
                     button.gameObject.SetActive(true);
                 }
+            }
+        }
+
+        private void OnCloseClick()
+        {
+            var popups = GameObject.FindGameObjectsWithTag("popup");
+            foreach (var popup in popups)
+            {
+                popup.SetActive(false);
             }
         }
     }
